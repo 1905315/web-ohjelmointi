7f4fd3064e549563eec20a765116e44c6654833f
Toni L <toni.m.luukkonen@outlook.com>
2022-10-22 14:17:23 +0300

    Käännöksen tallentamienn toimii, mutta tiedon haku ei

--- REST1/app.js
+++ REST1/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+app.use(express.static(__dirname));
+
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true }));
 
@@ -13,6 +15,35 @@ app.listen(3000, () => {
   console.log("Server listening at port 3000");
 });
 
+/*CORS isn’t enabled on the server, this is due to security reasons by default,
+so no one else but the webserver itself can make requests to the server.*/
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  res.setHeader("Content-type", "application/json");
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // GET a translation
 app.get("/dictionary/:finnishWord", (req, res) => {
   let englishWord = GetEnglishTranslation(req.params.finnishWord);
@@ -21,8 +52,9 @@ app.get("/dictionary/:finnishWord", (req, res) => {
 
 // ADD a translation
 app.post("/newTranslation/", (req, res) => {
-  let finnishWord = req.query.finnishWord;
-  let englishWord = req.query.englishWord;
+  let finnishWord = req.body.finnishWord;
+  let englishWord = req.body.englishWord;
+  console.log(finnishWord, englishWord);
   AddOrChangeTranslation(finnishWord, englishWord);
   res.json("Added or changed translation for " + finnishWord);
 });
